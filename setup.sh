d39edce1a1a7dea5127600e3924ef1c907c8f70e
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-06-04 15:50:12 +0200

    feat: add setup script for downloading and preparing experiment data

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Download the ground-truth.zip file
+curl -o data/ground-truth.zip https://zenodo.org/records/11472597/files/ground-truth.zip?download=1
+
+# Download the repos.zip file
+curl -o data/repos.zip https://zenodo.org/records/11472597/files/repos.zip?download=1
+
+# Unzip the ground-truth.zip file
+unzip data/ground-truth.zip -d data
+
+# Unzip the repos.zip file
+unzip data/repos.zip -d data
+
+# Clean zips
+rm data/ground-truth.zip
+rm data/repos.zip
