e9929515ce7a5a6f18e3a6e98e70008ebc7d4fc7
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-06-05 08:09:54 +0200

    refactor (analysis): apply formatter and remove deprecated jupyter notebook

--- python/generatePlotTransposed.py
+++ python/generatePlotTransposed.py
@@ -11,26 +11,37 @@ import ast
 import math
 
 # Parsing json data
+
+
 def parseDataRQ1(data):
     experiment = data.pop('experiment_properties')
     dataInSamples = {}
     l3 = data
     for l2, l2_data in l3.items():
         # Extract accuracy data
-        accuracy_data = {l1: [runs["accuracy"] for runs in l1_data.values()] for l1, l1_data in l2_data.items()}
+        accuracy_data = {l1: [runs["accuracy"] for runs in l1_data.values()]
+                         for l1, l1_data in l2_data.items()}
         # Extract precision data
-        precision_data = {l1: [runs["precision"] for runs in l1_data.values()] for l1, l1_data in l2_data.items()}
+        precision_data = {l1: [runs["precision"]
+                               for runs in l1_data.values()] for l1, l1_data in l2_data.items()}
         # Extract recall data
-        recall_data = {l1: [runs["recall"] for runs in l1_data.values()] for l1, l1_data in l2_data.items()}
+        recall_data = {l1: [runs["recall"] for runs in l1_data.values()]
+                       for l1, l1_data in l2_data.items()}
         # Extract f1-score data
-        f1_data = {l1: [runs["f1-score"] for runs in l1_data.values()] for l1, l1_data in l2_data.items()}
+        f1_data = {l1: [runs["f1-score"] for runs in l1_data.values()]
+                   for l1, l1_data in l2_data.items()}
 
-        accuracy_df = pd.DataFrame(accuracy_data, index=list(l2_data.values())[0].keys()).T
-        precision_df = pd.DataFrame(precision_data, index=list(l2_data.values())[0].keys()).T
-        recall_df = pd.DataFrame(recall_data, index=list(l2_data.values())[0].keys()).T
-        f1_score_df = pd.DataFrame(f1_data, index=list(l2_data.values())[0].keys()).T
-        
-        dataInSamples[l2]= {'accuracy_df':accuracy_df, 'precision_df':precision_df, 'recall_df':recall_df, 'f1_score_df':f1_score_df}
+        accuracy_df = pd.DataFrame(
+            accuracy_data, index=list(l2_data.values())[0].keys()).T
+        precision_df = pd.DataFrame(
+            precision_data, index=list(l2_data.values())[0].keys()).T
+        recall_df = pd.DataFrame(
+            recall_data, index=list(l2_data.values())[0].keys()).T
+        f1_score_df = pd.DataFrame(
+            f1_data, index=list(l2_data.values())[0].keys()).T
+
+        dataInSamples[l2] = {'accuracy_df': accuracy_df, 'precision_df': precision_df,
+                             'recall_df': recall_df, 'f1_score_df': f1_score_df}
     return experiment, dataInSamples
 
 
@@ -52,33 +63,40 @@ def plotLines(experiment, dataset_name, sample_size, average_df, accuracy_df, pr
 
     # Plot accuracy
     if plot_accuracy:
-        plot_dataframe('average accuracy improvement', accuracy_df, color[0], interpolation, marginAlpha, x, x_smooth)
+        plot_dataframe('average accuracy improvement', accuracy_df,
+                       color[0], interpolation, marginAlpha, x, x_smooth)
 
     # Plot precision
     if plot_precision:
-        plot_dataframe('average precision improvement', precision_df, color[1], interpolation, marginAlpha, x, x_smooth)
+        plot_dataframe('average precision improvement', precision_df,
+                       color[1], interpolation, marginAlpha, x, x_smooth)
 
     # Plot recall
     if plot_recall:
-        plot_dataframe('average recall improvement', recall_df, color[2], interpolation, marginAlpha, x, x_smooth)
+        plot_dataframe('average recall improvement', recall_df,
+                       color[2], interpolation, marginAlpha, x, x_smooth)
         # Plot the investment line
         # plt.plot([0, 5, 10, 15, 20, 25], [0, 5, 10, 15, 20, 25], linestyle='--', label='1-to-1 return on investment')
 
     # # Plot f1
     if plot_f_score:
-        plot_dataframe('average f1 improvement', f1_score_df, color[3], interpolation, marginAlpha, x, x_smooth)
+        plot_dataframe('average f1 improvement', f1_score_df,
+                       color[3], interpolation, marginAlpha, x, x_smooth)
 
-    plt.grid(True, which='both', linestyle='-', linewidth=0.5, color='gray')  # Normal grid
-    plt.xticks(x, [str(scenario) for scenario in x])  # Set x-axis tick locations
+    plt.grid(True, which='both', linestyle='-',
+             linewidth=0.5, color='gray')  # Normal grid
+    # Set x-axis tick locations
+    plt.xticks(x, [str(scenario) for scenario in x])
 
-    plt.ylim(0,100)
+    plt.ylim(0, 100)
     plt.title(title)
     if rq == 1:
         plt.xlabel('Percentage Mapping')
     plt.ylabel('Improvement in Percentage Points')
     plt.legend()
     plt.grid(True)
-    plt.savefig(fname='../results/plots/relative/' + dataset_name + '_lineplot_' + sample_size, dpi='figure', format=None)
+    plt.savefig(fname='../results/plots/relative/' + dataset_name +
+                '_lineplot_' + sample_size, dpi='figure', format=None)
     plt.show()
 
 
@@ -100,12 +118,13 @@ def plot_dataframe(label, accuracy_df, color, interpolation, marginAlpha, x, x_s
     y2th = y + 100 * accuracy_df.std(axis=0)
     f = interpolate.interp1d(x, y2th, kind=interpolation)
     y2th_smooth = f(x_smooth)
-    plt.fill_between(x_smooth, y1th_smooth, y2th_smooth, color=color, alpha=marginAlpha)
+    plt.fill_between(x_smooth, y1th_smooth, y2th_smooth,
+                     color=color, alpha=marginAlpha)
 
 
 # Plotting Bars function
 def plotBars(average_df, alpha=0.5, title='Test', rq=1):
-    color = ['blue','orange','green','red']
+    color = ['blue', 'orange', 'green', 'red']
     # Plotting the data
     average_df.plot(kind='bar', figsize=(10, 6), color=color, alpha=alpha)
     plt.title(title)
@@ -118,6 +137,8 @@ def plotBars(average_df, alpha=0.5, title='Test', rq=1):
     plt.show()
 
 # Plotting Boxes function
+
+
 def plotBoxs(data, title, ylabel, filename="Test", rq=1):
     data.boxplot(sym='o',               # Symbol to use for outliers
                  patch_artist=True,     # Whether to fill the boxes with colors
@@ -133,15 +154,19 @@ def plotBoxs(data, title, ylabel, filename="Test", rq=1):
                  boxprops=dict(color='blue'),          # Color of the box
                  whiskerprops=dict(color='green'),     # Color of the whiskers
                  capprops=dict(color='red'),           # Color of the caps
-                 medianprops=dict(color='orange'),     # Color of the median line
-                 flierprops=dict(markerfacecolor='purple', marker='o', markersize=8),  # Color and style of outliers
+                 # Color of the median line
+                 medianprops=dict(color='orange'),
+                 # Color and style of outliers
+                 flierprops=dict(markerfacecolor='purple',
+                                 marker='o', markersize=8),
                  )
     plt.title(title)
     plt.ylabel(ylabel)
     plt.ylim(0, 1)
     if rq == 1:
         plt.xlabel('Proactive Tracing Percentage')
-    plt.savefig(fname='../results/plots/final/' + filename + ".pdf", dpi='figure', format="pdf")
+    plt.savefig(fname='../results/plots/final/' +
+                filename + ".pdf", dpi='figure', format="pdf")
     plt.show()
 
 
@@ -160,7 +185,7 @@ def convert_to_relative_results(data: dict):
         avg_recall = average_dataframe(recall_df)
         avg_f1_score = average_dataframe(f1_score_df)
         averages = {"accuracy": avg_accuracy, "precision": avg_precision, "recall": avg_recall,
-                        "f1-score": avg_f1_score}
+                    "f1-score": avg_f1_score}
         average_df = pd.DataFrame(averages)
 
         # Remove the no longer valid average dataframe
@@ -223,7 +248,7 @@ def main():
             # averages with deviation?
             sample_size = sample_size.split()[0]
             # title = "Average recall improvement with " + sample_size + " variants"
-            #plotLines(experiment,
+            # plotLines(experiment,
             #          datasetName,
             #          sample_size,
             #          sampleData['average_df'],
@@ -232,10 +257,14 @@ def main():
 
             # boxplots
             title = ''
-            plotBoxs(sampleData['accuracy_df'], title=title, ylabel='Accuracy', filename="accuracy/" + datasetName + '-' + sample_size)
-            plotBoxs(sampleData['precision_df'], title=title, ylabel='Precision', filename="precision/" + datasetName + '-' + sample_size)
-            plotBoxs(sampleData['recall_df'], title=title, ylabel='Recall', filename="recall/" + datasetName + '-' + sample_size)
-            plotBoxs(sampleData['f1_score_df'], title=title, ylabel='F1 Score', filename="f1_score/" + datasetName + '-' + sample_size)
+            plotBoxs(sampleData['accuracy_df'], title=title, ylabel='Accuracy',
+                     filename="accuracy/" + datasetName + '-' + sample_size)
+            plotBoxs(sampleData['precision_df'], title=title, ylabel='Precision',
+                     filename="precision/" + datasetName + '-' + sample_size)
+            plotBoxs(sampleData['recall_df'], title=title, ylabel='Recall',
+                     filename="recall/" + datasetName + '-' + sample_size)
+            plotBoxs(sampleData['f1_score_df'], title=title, ylabel='F1 Score',
+                     filename="f1_score/" + datasetName + '-' + sample_size)
             print('------------------------------------------------------------------')
 
 
